fbe63453271a1cc32f6fc931865c1d5ff5b8b492
gooayanki <tup78242@temple.edu>
2025-09-20 10:40:29 +0800

    Initial Commit

---
+++ BIT/BITApp.swift
@@ -0,0 +1,17 @@
+//
+//  BITApp.swift
+//  BIT
+//
+//  Created by yanqi on 2025/9/20.
+//
+
+import SwiftUI
+
+@main
+struct BITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BITTests/BITTests.swift
@@ -0,0 +1,17 @@
+//
+//  BITTests.swift
+//  BITTests
+//
+//  Created by yanqi on 2025/9/20.
+//
+
+import Testing
+@testable import BIT
+
+struct BITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
